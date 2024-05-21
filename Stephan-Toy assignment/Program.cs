116ffe2a8986419d0bb3e10bdf6dc0cdcea91b15
Stephan <wearshark13@gmail.com>
2024-05-21 08:55:11 -0400

    Add visual studio project

---
+++ Stephan-Toy assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stephan_Toy_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
